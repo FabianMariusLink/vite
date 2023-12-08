970a442c4ae3e3e40530caff28b726e3f0003657
FabianMariusLink <fabian.marius.link@gmail.com>
2023-12-08 11:37:36 +0100

    Fix deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080'
-      }
+export default defineConfig(({command, mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        define: {
+            'process.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify(env.VITE_GOOGLE_MAPS_API_KEY),
+        },
+        plugins: [react()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:8080'
+                }
+            }
+        }
     }
-  }
 })
