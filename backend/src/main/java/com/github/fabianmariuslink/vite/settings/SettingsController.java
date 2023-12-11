114956f72187306f61099897a8654d2241bd0849
FabianMariusLink <fabian.marius.link@gmail.com>
2023-12-11 12:23:28 +0100

    Fix deployment

--- backend/src/main/java/com/github/fabianmariuslink/vite/settings/SettingsController.java
+++ backend/src/main/java/com/github/fabianmariuslink/vite/settings/SettingsController.java
@@ -1,18 +1,19 @@
 package com.github.fabianmariuslink.vite.settings;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/settings")
+@RequiredArgsConstructor
 public class SettingsController {
-    @Value("${vite.google.maps.api.key}")
-    private String googleMapsApiKey;
+
+    private final SettingsService settingsService;
 
     @GetMapping
     public String getGoogleMapsApiKey(){
-        return googleMapsApiKey;
+        return settingsService.getGoogleMapsApiKey();
     }
 }
